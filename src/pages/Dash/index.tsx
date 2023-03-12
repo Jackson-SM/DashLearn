c90ac6fefe3288b30589e5fb873a997e0730985f
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-03-12 15:13:25 -0300

    refactor: rebuilding dashboard page

--- src/pages/Dash/index.tsx
+++ src/pages/Dash/index.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import { CardInfo } from './CardInfo';
-import { CardsInfo, DashboardContainer, MainDash, SidebarDash } from './styles';
-import { BiGroup, BiListOl, BiNotepad, BiAlarm, BiChat, BiEnvelope } from 'react-icons/bi';
-import { CardsChart } from './CardsChart';
+import { DashboardContainer, MainDash, SidebarDash } from './styles';
 
 export const Dash = () => {
-  const cardInfos = [
-    { title: 'Friends', icon: <BiGroup />, amount: '7' },
-    { title: 'Tasks', icon: <BiListOl />, amount: '16' },
-    { title: 'Notes', icon: <BiNotepad />, amount: '4' },
-    { title: 'Inbox', icon: <BiEnvelope />, amount: '36' },
-    { title: 'Talks', icon: <BiChat />, amount: '5' },
-    { title: 'Hours', icon: <BiAlarm />, amount: '10' },
-  ];
-
   return (
     <DashboardContainer>
       <MainDash>
-        <CardsInfo>
-          {cardInfos.map((info) => {
-            return <CardInfo key={info.title} info={info} />;
-          })}
-        </CardsInfo>
-        <CardsChart />
+        <h1>MainDash</h1>
       </MainDash>
       <SidebarDash>
         <h1>sidebar</h1>
